c89e3a491972696af845dccfdcbd2eaaba604b0e
Denis Smolonski <denissmolonski@gmail.com>
2025-03-11 15:38:43 +0100

    feat: update link new suggestion form

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -93,7 +93,7 @@ export const HelperCenterArticleTitles = {
 }
 
 export const NEW_SUGGESTION_FORM =
-  'https://docs.google.com/forms/d/e/1FAIpQLSfojsADYCiWq9AqbLqsUTzCDSpA8FMgdAQp0Pyl0BOeurlq9A/viewform'
+  'https://safe-support.protofire.io/'
 export const RECOVERY_FEEDBACK_FORM =
   'https://noteforms.com/forms/safe-feedback-form-hk16ds?notionforms=1&utm_source=notionforms'
 
