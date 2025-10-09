f651df16f0d2fe67ec6c3f05a4e0bbd6680c73a3
ajimeno04 <jimenobeltran11@gmail.com>
2025-10-09 13:53:40 +0200

    refactor: simplify SunsetWarning title and improve text formatting

--- apps/web/src/features/warningBanner/SunsetWarning.tsx
+++ apps/web/src/features/warningBanner/SunsetWarning.tsx
@@ -10,18 +10,15 @@ export const SunsetWarning = () => {
   if (!isFeatureEnabled) return null
 
   return (
-    <ErrorMessage level="warning" title={
-      <>
-        Botanix is now available on the official Safe app at{' '}
-        <ExternalLink href="https://app.safe.global/">app.safe.global</ExternalLink>!
-      </>
-    }>
+    <ErrorMessage level="warning" title="Botanix is now available on the official Safe app at app.safe.global!">
       <Typography display="inline" mr={1}>
-        Created safes are already available there; to transfer local data (address book, settings, and variables), use the Export/Import functionality found on the Settings > Data page{' '}
+        Created safes are already available there; to transfer local data (address book, settings, and variables), use
+        the Export/Import functionality found on the Settings &gt; Data page{' '}
         <ExternalLink href="https://safe.botanixlabs.com/settings/data">here</ExternalLink>.
       </Typography>
       <Typography display="inline">
-        Please complete your unfinished transactions here until the November 1, 2025 since they won't be migrated to Safe Global version.
+        Please complete your unfinished transactions here until the November 1, 2025 since they won&apos;t be migrated
+        to Safe Global version.
       </Typography>
     </ErrorMessage>
   )
