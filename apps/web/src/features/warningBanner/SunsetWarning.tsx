d5c5b5879499b4855c9c530a0a7f449224e619a0
ajimeno04 <jimenobeltran11@gmail.com>
2025-10-09 13:18:40 +0200

    feat: add SunsetWarning component and update FEATURE enum

---
+++ apps/web/src/features/warningBanner/SunsetWarning.tsx
@@ -0,0 +1,28 @@
+import ErrorMessage from '@/components/tx/ErrorMessage'
+import { Typography } from '@mui/material'
+import ExternalLink from '@/components/common/ExternalLink'
+import { useHasFeature } from '@/hooks/useChains'
+import { FEATURES } from '@/utils/chains'
+
+export const SunsetWarning = () => {
+  const isFeatureEnabled = useHasFeature(FEATURES.SUNSET_BANNER)
+
+  if (!isFeatureEnabled) return null
+
+  return (
+    <ErrorMessage level="warning" title={
+      <>
+        Botanix is now available on the official Safe app at{' '}
+        <ExternalLink href="https://app.safe.global/">app.safe.global</ExternalLink>!
+      </>
+    }>
+      <Typography display="inline" mr={1}>
+        Created safes are already available there; to transfer local data (address book, settings, and variables), use the Export/Import functionality found on the Settings > Data page{' '}
+        <ExternalLink href="https://safe.botanixlabs.com/settings/data">here</ExternalLink>.
+      </Typography>
+      <Typography display="inline">
+        Please complete your unfinished transactions here until the November 1, 2025 since they won't be migrated to Safe Global version.
+      </Typography>
+    </ErrorMessage>
+  )
+}
